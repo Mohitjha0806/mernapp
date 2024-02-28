024c0295175143c39df6b161766ae4bd28ad3741
Mohit Jha <your.email@example.com>
2024-02-28 20:54:19 +0530

    modified:   src/App.js 	new file:   src/components/Footer.js 	new file:   src/components/Navbar.js 	new file:   src/screens/Home.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './screens/Home';
 
 function App() {
   return (
-    <div className="fs-1" >Header</div>
-  );
+    <>
+    <div> <Home></Home></div>
+    </>);
 }
 
 export default App;
