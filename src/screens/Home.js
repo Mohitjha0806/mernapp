35221a7fef1e59ea2bd9330d19d6caae87537035
Mohit Jha <your.email@example.com>
2024-03-04 00:48:41 +0530

    work on home till 20-37

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,7 +8,17 @@ export default function Home() {
       <div>
         <Navbar />
       </div>
-      <div>Body</div>
+      <div>
+        <div className="card m-3 " style={{ width: "18rem", "maxHeight":"360px"}}>
+          <img className="card-img-top" src="..." alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is some important text.
+            </p>
+          </div>
+        </div>
+      </div>
       <div>
         <Footer />
       </div>
