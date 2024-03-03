8d40ee911140a01d61c81335cb130a3c4db2ec8e
Mohit Jha <your.email@example.com>
2024-03-04 00:09:24 +0530

    change in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar() {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Features
+                  About
                 </a>
               </li>
               <li className="nav-item">
